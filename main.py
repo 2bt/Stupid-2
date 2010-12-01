c79c05e13abd10348230c77af2dd6fccd6894098
twobit <twobit@gmx.net>
2010-12-01 11:41:18 +0100

    projection matrix done

--- main.py
+++ main.py
@@ -14,17 +14,44 @@ from vec import vec
 
 
 
+class Contra(Entity):
+
+	def __init__(self):
+		Entity.__init__(self)
+		self.cube = shit.Brick(1, 1, 1).gen_list()
+
+
+	def render_sub(self):
+
+		glPushMatrix()
+		q = vec(0, 0, -0.5)
+		glMultMatrixf([
+			1, 0, 0, q.x,
+			0, 1, 0, q.y,
+			0, 0, 1, q.z,
+			0, 0, 0, 1
+		])
+		glColor(1, 1, 0)
+		glCallList(self.cube)
+		glPopMatrix()
+
+		glColor(1, 1, 1, 0.4)
+		glCallList(self.cube)
+
+
 class Suck(Entity):
 
 	def __init__(self):
 		Entity.__init__(self)
 
-		self.orb = shit.Orb(5).gen_list()
+		self.orb = shit.Orb(1).gen_list()
+		self.r = 0
 
 	def update_sub(self, time_dif):
-		pass
+		self.r += time_dif * 10
 
 	def render_sub(self):
+		glRotate(self.r, 0, 1, 0)
 		glCallList(self.orb)
 
 
@@ -32,6 +59,7 @@ class Suck(Entity):
 if __name__ == "__main__":
 
 	stupid = Stupid("Stupid [ CGI - Aufgabe 2 ]")
+	stupid.add(Contra())
 	stupid.add(Suck())
 	stupid.mainloop()
 
