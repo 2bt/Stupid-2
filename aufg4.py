38950fa90b2cac83829b25c39fd0424ee6990494
twobit <twobit@gmx.net>
2011-01-13 05:53:43 +0100

    golf

--- aufg4.py
+++ aufg4.py
@@ -9,8 +9,8 @@ class W(Entity):
  def l(S,t):return sum((x*reduce(float.__mul__,((t*~-l-k)/(i-k+.0)for k in r if i-k))for i,x in zip(r,p)),v)
  def render(S):
 	S.f^=s.key_state(K_t,1);D(2896);D(2929);glLineWidth(5);C(.3,.3,.25)
-	for x in p:T(*x);glCallList(o);T(*-x)
+	for x in p:T(*x);o.render();T(*-x)
 	B(3);map(V,p);glEnd();B(3);C(*v);f=[S.l,S.b][S.f];d=a=f(0);V(a);t=0
-	while t<1:t+=.005;b=f(t);c=b-a;C(0,0,1-c.normalize().dot(d)**150);V(b);a=b;d=c
+	while t<1:t+=.01;b=f(t);c=b-a;C(0,0,1-c.normalize().dot(d)**150);V(b);a=b;d=c
 	glEnd()
-s=Stupid("");o=Orb(.1,3).gen_list();s.add(W());s.mainloop()
+s=Stupid("");o=Orb(.1,3);o.gen_list();s.add(W());s.mainloop()
